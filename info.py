fc19bc5c472c9190c4d575bf3986949f71fd9255
malan88 <m@stdwtr.io>
2020-09-29 15:49:18 -0400

    Add underscore and asterisk counters to info.py

--- info.py
+++ info.py
@@ -12,6 +12,16 @@ def main(filename):
     charlist.sort()
     charstring = ''.join(charlist)
     print(size, charstring)
+    if '_' in charstring:
+        count = text.count('_')
+        print(f"{count} underscores were found in the text.")
+        print("An even count implies they are used as markup.")
+    if '*' in charstring:
+        count = text.count('*')
+        print(f"{count} asterisks were found in the text.")
+        print("An even count implies they could be markup.")
+        print("Visually verify they are not being used as annotations.")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Get unique characters and size"
